7fffb1d7c184648bb82a84001e15cede28e6bbda
as <121367462+asucas@users.noreply.github.com>
2023-07-25 20:32:37 +0800

    Update youtube.ads

--- youtube.ads
+++ youtube.ads
@@ -1,6 +1,6 @@
-#!name=youtube
+#!name=youtube20.33
 
-#!desc=去除部分插播广告
+#!desc=去除部分插播广告-没有pip
 
 [Script]
 youtube = type=http-response,pattern=^https:\/\/youtubei\.googleapis\.com\/youtubei\/v1\/(browse|next|player|search|reel\/reel_watch_sequence|guide|account\/get_setting)\?,requires-body=1,binary-body-mode=1,max-size=2097152,script-path=https://raw.githubusercontent.com/Maasea/sgmodule/master/Script/Youtube/youtube.js
